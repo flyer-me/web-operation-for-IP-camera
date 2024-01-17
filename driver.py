fbf917640d69a18191ae438a8183761c84b30dd1
flyer-me <wup3306@gmail.com>
2024-01-17 15:25:43 +0800

    Add files via upload

--- driver.py
+++ driver.py
@@ -8,6 +8,12 @@ from selenium.webdriver.chrome.service import Service
 # "C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222
 
 def init():
+    """
+    Initializes the webdriver with specified options and settings.
+
+    Returns:
+        driver (WebDriver): The initialized webdriver instance.
+    """
     path = Service('chromedriver.exe')
     options = Options()
     options.add_argument('disable-notifications')
@@ -23,24 +29,51 @@ def init():
     return driver
 
 
-def _get_attribute_of_element(element,name='textContent') -> str:
+def _get_attribute_of_element(element, name='textContent') -> str:
+    """
+    获取元素的属性值。
+
+    Args:
+        element: 元素对象。
+        name: 属性名称，默认为'textContent'。
+
+    Returns:
+        元素的属性值。
+
+    Raises:
+        Exception: 获取属性值失败。
+    """
     global fail_count
     try:
-        return element.get_attribute(name) #不要使用innerText
+        return element.get_attribute(name)  # 不要使用innerText
     except Exception:
         fail_count = fail_count + 1
         raise Exception("_get_attribute失败")
 
-def _click(driver,value: str,by=By.XPATH) -> bool:
+def _click(driver, value: str, by=By.XPATH) -> bool:
+    """
+    根据给定的定位值和定位策略，点击元素。
+
+    Args:
+        driver: The WebDriver instance.
+        value (str): 定位所需str的值.
+        by (By): 定位策略,默认为By.XPATH.
+
+    Returns:
+        bool: 成功则返回True,否则raise exception.
+
+    Raises:
+        Exception: 点击失败.
+    """
     global fail_count
     try:
-        element = driver.find_element(by,value=value)
+        element = driver.find_element(by, value=value)
         try:
             element.click()
             time.sleep(0.01)
         except Exception:
-            driver.execute_script("arguments[0].scrollIntoView();",element)
-            driver.execute_script("(arguments[0]).click();",element)
+            driver.execute_script("arguments[0].scrollIntoView();", element)
+            driver.execute_script("(arguments[0]).click();", element)
     except Exception:
         fail_count = fail_count + 1
         raise Exception(f"点击value={value},by={str(by)}失败")

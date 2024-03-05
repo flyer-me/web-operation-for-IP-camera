9e9d529031c5197321b5769c2d4c90b6124f31de
flyer-me <fei2704247912@qq.com>
2024-03-05 11:36:10 +0800

    Update DrissionPage_test.py
    Signed-off-by: flyer-me <fei2704247912@qq.com>

--- DrissionPage_test.py
+++ DrissionPage_test.py
@@ -1,8 +1,32 @@
 from DrissionPage import SessionPage
 
+from DrissionPage import ChromiumOptions, SessionOptions, WebPage
+
+co = ChromiumOptions(ini_path=r'.\config\web-config.ini')
+so = SessionOptions(ini_path=r'.\config\web-config.ini')
+
+page = WebPage(chromium_options=co, session_or_options=so)
 # 创建页面对象
 page = SessionPage()
 
+"""
+# 获取 id 为 one 的元素
+div1 = page.ele('#one')
+# 获取 name 属性为 row1 的元素
+p1 = page.ele('@name=row1')
+# 获取包含“第二个div”文本的元素
+div2 = page.ele('第二个div')
+# 获取所有div元素
+div_list = page.eles('tag:div')
+# 获取到一个元素div1
+div1 = page.ele('#one')
+
+# 在div1内查找所有p元素
+p_list = div1.eles('tag:p')
+
+# 获取div1后面一个元素
+div2 = div1.next()
+"""
 # 爬取3页
 for i in range(1, 2):
     # 访问某一页的网页

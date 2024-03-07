89d4c6618188a064e88a8895227f56b794ca2910
flyer <flyer558@163.com>
2024-03-07 18:01:26 +0800

    add base file.

--- DrissionPage_test.py
+++ DrissionPage_test.py
@@ -7,7 +7,7 @@ so = SessionOptions(ini_path=r'.\config\web-config.ini')
 
 page = WebPage(chromium_options=co, session_or_options=so)
 # 创建页面对象
-page = SessionPage()
+#page = SessionPage()
 
 """
 # 获取 id 为 one 的元素
@@ -27,13 +27,19 @@ p_list = div1.eles('tag:p')
 # 获取div1后面一个元素
 div2 = div1.next()
 """
-# 爬取3页
-for i in range(1, 2):
-    # 访问某一页的网页
-    page.get(f'https://gitee.com/explore/all?page={i}')
-    # 获取所有开源库<a>元素列表
-    links = page.eles('.title project-namespace-path')
-    # 遍历所有<a>元素
-    for link in links:
-        # 打印链接信息
-        print(link.text, link.link)
+
+ip = '172.16.x.x'
+username = 'admin'
+passwd = 'xx'
+# 访问某一页的网页
+page.get(f'http://{ip}')
+page.ele(locator='#username').input(username)
+page.ele(locator='#password').input(passwd)
+page.ele(locator='@type=button').click()
+"""
+links = page.eles('.title project-namespace-path')
+# 遍历所有<a>元素
+for link in links:
+    # 打印链接信息
+    print(link.text, link.link)
+"""
